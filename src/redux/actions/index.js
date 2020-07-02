c3b0a8259c81a501ce9c0876183b5fd45d45e618
Андрей Загоненко <cequeditlabouchedombre@gmail.com>
2020-07-02 17:36:00 +0300

    refactored actions and reducer's logic

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,7 +1,7 @@
 import DataService from "../../services/DataService";
 
 export const GET_DATA_SUCCESS = "GET_DATA_SUCCESS";
-export const CHANGE_ELEMENT_DATA = "CHANGE_ELEMENT_DATA";
+export const UPDATE_DATA_WITH_CHANGED_ELEMENT = "UPDATE_DATA_WITH_CHANGED_ELEMENT";
 
 export const getDataSuccess = data => ({
     type: GET_DATA_SUCCESS,
@@ -9,20 +9,58 @@ export const getDataSuccess = data => ({
 });
 
 export const getData = () => {
-    return (dispatch) => {
+    return (dispatch, getState) => {
         return DataService.getData()
             .then(response => {
-                dispatch(getDataSuccess(response.data));
+                const prevData = getState().dataReducer.data;
+                if (prevData.length && prevData.some(elem => elem.changed)) {
+                    const filteredData = response.data.map((elem, index) => {
+                        const prevElemChangedValue = prevData[index]
+                        return prevElemChangedValue && prevElemChangedValue.changed ? {
+                            ...elem,
+                            stocks: {
+                                NASDAQ: typeof prevElemChangedValue.stocks.NASDAQ === "number" ? prevElemChangedValue.stocks.NASDAQ : elem.stocks.NASDAQ,
+                                CAC40: typeof prevElemChangedValue.stocks.CAC40 === "number"  ? prevElemChangedValue.stocks.CAC40 : elem.stocks.CAC40
+                            },
+                            changed: prevElemChangedValue.changed
+                        } : elem
+                    })
+                    dispatch(getDataSuccess(filteredData));
+                } else {
+                    dispatch(getDataSuccess(response.data));
+                }
                 return response;
             })
     };
 }
 
+export const updateDataWithChangedElement = (updatedData) => ({
+    type: UPDATE_DATA_WITH_CHANGED_ELEMENT,
+    data: updatedData
+})
+
 export const changeElementData = (value, name, index) => {
-    return {
-        type: CHANGE_ELEMENT_DATA,
-        value,
-        name,
-        index
+    return (dispatch, getState) => {
+        const prevData = getState().dataReducer.data;
+        const updatedData = prevData.map((e, i) => {
+            if (i === index) {
+                let updatedElem = {
+                    ...e,
+                    stocks: {
+                        ...e.stocks,
+                        [name]: value,
+                    },
+                }
+                if (e.changed) {
+                    e.changed.includes(name) ? updatedElem.changed = [...e.changed] : updatedElem.changed = [...e.changed, name]
+                } else {
+                    updatedElem.changed = [name]
+                }
+                return updatedElem
+            }
+
+            return e;
+        })
+        dispatch(updateDataWithChangedElement(updatedData))
     }
 }
--- src/redux/reducers/dataReducer.js
+++ src/redux/reducers/dataReducer.js
@@ -1,4 +1,4 @@
-import { GET_DATA_SUCCESS, CHANGE_ELEMENT_DATA } from "../actions";
+import { GET_DATA_SUCCESS, UPDATE_DATA_WITH_CHANGED_ELEMENT } from "../actions";
 
 const initialState = {
     data: [],
@@ -8,56 +8,14 @@ const initialState = {
 const rootReducer = (state = initialState, action) => {
     switch (action.type) {
         case GET_DATA_SUCCESS:
-            if (state.data.length && state.data.some(e => e.changed)) {
-                const filteredData = action.data.map((e, i) => {
-                    const prevElemChangedValue = state.data.find((elem, index) => index === i)
-                    if (prevElemChangedValue && prevElemChangedValue.changed) {
-                        return {
-                            ...e,
-                            stocks: {
-                                NASDAQ: typeof prevElemChangedValue.stocks.NASDAQ === "number" ? prevElemChangedValue.stocks.NASDAQ : e.stocks.NASDAQ,
-                                CAC40: typeof prevElemChangedValue.stocks.CAC40 === "number"  ? prevElemChangedValue.stocks.CAC40 : e.stocks.CAC40
-                            },
-                            changed: prevElemChangedValue.changed
-                        }
-                    }
-                    return e
-                })
-
-                return {
-                    ...state,
-                    data: filteredData
-                }
-            }
             return {
                 ...state,
                 data: action.data
             }
-        case CHANGE_ELEMENT_DATA:
-            const { name, value, index } = action;
-            const updatedData = state.data.map((e, i) => {
-                if (i === index) {
-
-                    let updatedElem = {
-                        ...e,
-                        stocks: {
-                            ...e.stocks,
-                            [name]: value,
-                        },
-                    }
-                    if (e.changed) {
-                        e.changed.includes(name) ? updatedElem.changed = [...e.changed] : updatedElem.changed = [...e.changed, name]
-                    } else {
-                        updatedElem.changed = [name]
-                    }
-                    return updatedElem
-                }
-
-                return e;
-            })
+        case UPDATE_DATA_WITH_CHANGED_ELEMENT:
             return {
                 ...state,
-                data: updatedData
+                data: action.data
             }
         default:
             return state
